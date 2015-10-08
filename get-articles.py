aced34b854c1619d09171e1ac80e433f8cf5b48a
Jeff <jeffreyh192@gmail.com>
2015-10-08 00:41:37 -0700

    got title, link, comment link scraping workingls!

--- get-articles.py
+++ get-articles.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3.4
 from bs4 import BeautifulSoup
+from collections import namedtuple
 import requests
 import sys
+
+Article = namedtuple('Article', ['title', 'link', 'comments_link'])
 
 def getPageContent(url):
     # Reddit uses user-agent id as a rudimentary bot checking mech.
@@ -9,70 +12,40 @@ def getPageContent(url):
     page = requests.get(url, headers = user_agent)
     return page.content
 
+def getArticles(reddit_page):
+    articles = []
+
+    soup = BeautifulSoup(reddit_page)
+
+    # Find Articles Table
+    table = soup.find('div', {'id':'siteTable'})
+    for art in table:
+        # Only get articles that have a rank assoc with them
+        # "real" articles
+        res = art.find('span', {'class':'rank'})
+        if res is not None and res.string is not None:
+            res1 = art.find_all('p', {'class', 'title'})
+            res2 = art.find_all('a', {'class', 'comments'})
+            title = res1[0].a.string
+            link = res1[0].a['href']
+            comments_link = res2[0]['href']
+            articles.append(Article(title=title, link=link, comments_link=comments_link))
+
+    return articles
+
+
 if __name__ == "__main__":
-#     url="https://www.reddit.com/r/askreddit/"
-#     page_content = getPageContent(url)
-#     with open('content.html', 'a+') as f:
-#         f.write(str(page_content))
-#
-    with open('content.html', 'r') as f:
-        text = f.read()
-        soup = BeautifulSoup(text)
-        print("--- SOUP --- ")
-        table = soup.find('div', {'id':'siteTable'})
-        count = 0
-        for art in table:
-            res = art.find_all('p', {'class', 'title'})
-            if len(res) == 1:
-                count += 1
-                print(res[0])
-                print("")
-        print("num results found: ", count)
-#             res = art.find('div', {'class':'title'})
-#             print(type(res))
-#             res = art.find('p', {'class':'title'})
-#             print(type(res))
-#             print(res.prettify())
-#         print("--- SOUP HEADER ---")
-#         res = soup.header
-#         print(type(res))
-#         print(res.prettify())
-#         print("--- SOUP FIND ALL ---")
-#         res = soup.findAll('h1', {'class':'hover redditname'})
-#         print(type(res))
-#         print(len(res))
-#         print(res)
-#         res = soup.findAll('h2', {'class':'result-message'})
-#         print(type(res))
-#         print(len(res))
-#         print(res)
-#         res = soup.findAll('div', {'class':'subscribe-thanks'})
-#         print(type(res))
-#         print(len(res))
-#         print(res)
-#         print("--- SOUP HEADER FIND ALL ---")
-#         res = soup.header.findAll('h1', {'class':'subscribe-callout'})
-#         print(type(res))
-#         print(len(res))
-#         print(res)
-#         res = soup.header.findAll('h2', {'class':'result-message'})
-#         print(type(res))
-#         print(len(res))
-#         print(res)
-#         res = soup.header.findAll('div', {'class':'subscribe-thanks'})
-#         print(type(res))
-#         print(len(res))
-#         print(res)
-#         print("--- SOUP HEADER FIND_ALL ---")
-#         res = soup.header.find_all('h1', {'class':'subscribe-callout'})
-#         print(type(res))
-#         print(len(res))
-#         print(res)
-#         res = soup.header.find_all('h2', {'class':'result-message'})
-#         print(type(res))
-#         print(len(res))
-#         print(res)
-#         res = soup.header.find_all('div', {'class':'subscribe-thanks'})
-#         print(type(res))
-#         print(len(res))
-#         print(res)
+    url="https://www.reddit.com/r/showerthoughts/?count=25"
+    page_content = getPageContent(url)
+    articles = getArticles(page_content)
+
+#     with open('content.html', 'r') as f:
+#         text = f.read()
+#         articles = getArticles(text)
+
+    print("Number of articles found: ", len(articles))
+    for a in articles:
+        print("title:    ", a.title)
+        print("link:     ", a.link)
+        print("comments: ", a.comments_link)
+        print("")
