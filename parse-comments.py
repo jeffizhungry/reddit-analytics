f9c1e3f1900d2b4d4fb169e9b932be51fc382c0f
Jeff <jeffreyh192@gmail.com>
2015-10-20 21:39:47 -0700

    forgot to add file on last commit

---
+++ parse-comments.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.4
+import selenium
+
+if __name__ == "__main__":
+    url=("https://www.reddit.com/r/Showerthoughts/comments/3ph5vg/"
+         "the_usa_doesnt_have_a_name_for_their_country_they/")
+
+
+
